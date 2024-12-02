eb9b03e199869f07b9cff426e8e43831f6b2e8cf
BBrannick <bbrannick@gocardless.com>
2024-12-02 20:05:54 +0000

    2024 day 1 and 2 solutions

---
+++ 2024/01/01.hs
@@ -0,0 +1,66 @@
+import System.IO
+import Data.List
+import Data.Maybe
+import qualified Data.Map as Map
+
+
+-- Boilerplate
+main :: IO ()
+main = do
+  run "full.txt" main'
+
+demo :: IO ()
+demo = do
+  run "demo.txt" main'
+
+run filename fn = do
+  ls <- getLines filename
+  print $ fn ls
+
+getLines :: String -> IO ([String])
+getLines filename = do
+  handle <- openFile filename ReadMode
+  contents <- hGetContents handle
+  return $ lines contents
+
+
+-- Main
+main' = pt2
+
+pt1 :: [String] -> Int
+pt1 lines = sum $ map (uncurry diff) (sortedPairs (map parse lines))
+  
+
+diff :: Num a => a -> a -> a
+diff x y = abs (x-y) 
+
+sortedPairs :: [(Int,Int)] -> [(Int,Int)]
+sortedPairs pairs = zip sortedLeft sortedRight
+  where (left, right) = unzip pairs
+        sortedLeft = sort left
+        sortedRight = sort right
+
+parse :: String -> (Int, Int)
+parse line = (a,b)
+  where a:b:_ = map (read::String->Int) $ words line 
+
+
+
+--
+
+pt2 :: [String] -> Int
+pt2 lines = sum (map (similarity tallyMap) listL)
+  where (listL, listR) = unzip $ map parse lines
+        tallyMap = tally listR
+
+similarity :: Map.Map Int Int -> Int -> Int
+similarity m x = x * (fromMaybe 0 (Map.lookup x m))
+
+tally :: Ord a => [a] -> Map.Map a Int
+tally = tally' Map.empty
+
+tally' :: Ord a => Map.Map a Int -> [a] -> Map.Map a Int
+tally' m [] = m
+tally' m (x:xs) = Map.insertWith (+) x 1 $ tally' m xs
+
+
---
+++ 2024/02/02.hs
@@ -0,0 +1,67 @@
+import System.IO
+import Data.List
+import Data.Maybe
+import qualified Data.Map as Map
+
+
+-- Boilerplate
+main :: IO ()
+main = do
+  run "full.txt" main'
+
+demo :: IO ()
+demo = do
+  run "demo.txt" main'
+
+run filename fn = do
+  ls <- getLines filename
+  print $ fn ls
+
+getLines :: String -> IO ([String])
+getLines filename = do
+  handle <- openFile filename ReadMode
+  contents <- hGetContents handle
+  return $ lines contents
+
+
+-- Main
+main' = pt2
+
+pt1 :: [String] -> Int
+pt1 = length . filter isSafe . map parse 
+
+parse :: String -> [Int]
+parse = map (read::String->Int) . words 
+
+isSafe :: [Int] -> Bool
+isSafe [] = True
+isSafe (x:[]) = True
+isSafe report@(x:y:rest)
+  | x < y = isSafe' safeInc report
+  | x > y = isSafe' safeDec report
+  | otherwise = False
+
+isSafe' _ [] = True
+isSafe' _ (x:[]) = True
+isSafe' cond (x:y:rest) = (cond x y) && isSafe' cond (y:rest)
+
+safeInc :: Int -> Int -> Bool
+safeInc x y = x < y && y <= (x+3)
+
+safeDec :: Int -> Int -> Bool
+safeDec x y = x > y && y >= (x-3)
+
+-- Pt 2
+pt2 :: [String] -> Int
+pt2 = length . filter isSafeish . map parse
+
+
+isSafeish :: [Int] -> Bool
+isSafeish reports = isSafe reports || any isSafe (sublists reports)
+
+sublists :: [Int] -> [[Int]]
+sublists xs = sublists' (length xs) xs
+
+sublists' :: Int -> [Int] -> [[Int]]
+sublists' 0 xs = []
+sublists' n xs = ((take (n-1) xs) ++ (drop n xs)):(sublists' (n-1) xs)
