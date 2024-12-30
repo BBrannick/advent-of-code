63c14cc7d82ab0ed05766f2ab236f91862905504
Ben Brannick <benjbrannick@gmail.com>
2024-12-30 19:25:39 +0000

    Day 13 part 2 solution

--- 2024/app/Day13.hs
+++ 2024/app/Day13.hs
@@ -10,52 +10,76 @@ type Coord = (Int,Int)
 type Mach = (Coord, Coord, Coord)
 
 pt1 :: String -> Int
-pt1 s = sum . catMaybes . map cost . parse $ s
+pt1 = sum . catMaybes . map cost . parse 0
 
+-- Solved with help from https://advent-of-code.xavd.id/writeups/2024/day/13/
 pt2 :: String -> Int
-pt2 = length
+pt2 = sum . catMaybes . map cost . parse 10000000000000
 
 cost :: Mach -> Maybe Int
-cost (a,b@(x,y),tgt@(x',y')) = maybeMin .
-  catMaybes . 
-    map (\(b,ma) -> fmap (+b) ma) .
-    zip [0..] .
-      map (costA a) $
-        (zip [x',(x'-x)..0] [y',(y'-y)..0])
+cost ((ax,ay),(bx,by),(tx,ty)) = if a_rem == 0 && b_rem == 0 
+                                    then Just $ (3*a_presses) + b_presses 
+                                    else Nothing
+  where (a_presses,a_rem) = divMod ((tx*by)-(ty*bx)) ((ax*by)-(ay*bx))
+        (b_presses,b_rem) = divMod (tx - (a_presses * ax)) bx
 
-maybeMin :: Ord a => [a] -> Maybe a
-maybeMin [] = Nothing
-maybeMin xs = Just (minimum xs)
+  {- Initial idea was here
+      Idea was to find the intersection of B intersecting with (0,0), and A 
+      intersecting with the target, then check that B could reach that point in 
+      an integer number of presses, and A could reach the target in an integer
+      number of presses.
+      That answer was too low, I'm still not certain why, but the above
+      solution is cleaner anyway. I've left this here for posterity
+      -}
+_fullCost :: Mach -> Maybe Int
+_fullCost (a,b,t) = case (costIA,costBI) of
+                     (Just ca, Just cb) -> Just (ca+cb)
+                     _ -> Nothing
+  where is = intersection a b t
+        costBI = cost' 1 (0,0) b is
+        costIA = cost' 3 is a t
 
-costA :: Coord -> Coord -> Maybe Int
-costA (x,y) (tgtX,tgtY)
-  | rx == 0 && ry == 0 && dx == dy = Just (3*dx)
+-- Find the cost of pressing button to move between two coords, or nothing if impossible
+cost' :: Int -- cost of one button press
+      -> Coord -- starting coordinate
+      -> Coord -- vector
+      -> Coord -- target coordinate
+      -> Maybe Int -- cost if possible, nothing otherwise
+cost' c (x,y) (vx,vy) (tx,ty)
+  | rx == 0 && ry == 0 && dx == dy = Just (c*dx)
   | otherwise = Nothing
-  where (dx,rx) = divMod tgtX x 
-        (dy,ry) = divMod tgtY y
+  where tgtX = tx-x
+        tgtY = ty-y 
+        (dx,rx) = divMod tgtX vx 
+        (dy,ry) = divMod tgtY vy
 
-
-tu :: (a -> b -> c) -> (a,a) -> (b,b) -> (c,c)
-tu f (x,y) (x',y') = (f x x', f y y')
+intersection :: Coord -> Coord -> Coord -> Coord
+intersection (ax,ay) (bx,by) (tx,ty) = (ix,iy)
+  where denom = (fromIntegral::Int->Double) $ (by*ax)-(ay*bx)
+        c = (fromIntegral ty) - ((fromIntegral tx) * (((fromIntegral::Int->Double) ay)/(fromIntegral ax)))
+        ix = floor $ ((fromIntegral bx)*(fromIntegral ax)*c) / denom
+        iy = floor $ ((fromIntegral by)*(fromIntegral ax)*c) / denom
 
 --- PARSING LOGIC
 
-parse :: String -> [Mach]
-parse = map parseMach . init . splitOn "\n\n"
+parse :: Int -> String -> [Mach]
+parse offset = map (parseMach offset) . init . splitOn "\n\n"
 
-parseMach :: String -> Mach
-parseMach s = (a,b,tgt)
-  where [a,b,tgt] = zipWith ($) [parseBtn, parseBtn, parseTgt] (lines s)
+parseMach :: Int -> String -> Mach
+parseMach offset s = case zipWith ($) [parseBtn, parseBtn, parseTgt offset] (lines s) of
+                       [a,b,t] -> (a,b,t)
+                       _ -> error "failed to parse"
 
 parseBtn :: String -> Coord
 parseBtn = parseLn (mkRegex "X\\+([0-9]+), Y\\+([0-9]+)")
 
-parseTgt :: String -> Coord
-parseTgt = parseLn (mkRegex "X=([0-9]+), Y=([0-9]+)")
+parseTgt :: Int -> String -> Coord
+parseTgt offset s = (x+offset,y+offset)
+  where (x,y) = parseLn (mkRegex "X=([0-9]+), Y=([0-9]+)") s
 
 parseLn :: Regex -> String -> Coord
 parseLn r s = case matchRegex r s of
                Just [x,y] -> ((read::String->Int) x, (read::String->Int) y)
-               _ -> (0,0) -- shouldn't happen
+               _ -> error "failed to parse"
 
 
--- 2024/app/Main.hs
+++ 2024/app/Main.hs
@@ -1,16 +1,13 @@
 module Main where
 
-import Day16 (pt1, pt2)
+import Day13 (pt1, pt2)
 
 main :: IO ()
 main = do
-  demoData <- readFile "data/16_demo1.txt"
-  demo2Data <- readFile "data/16_demo2.txt"
-  realData <- readFile "data/16.txt"
+  demoData <- readFile "data/13_demo.txt"
+  realData <- readFile "data/13.txt"
   print $ pt1 demoData
-  print $ pt1 demo2Data
   print $ pt1 realData
   print $ pt2 demoData
-  print $ pt2 demo2Data
   print $ pt2 realData
 
