d3f25d576ea6b879b4db99b1e74b7e3f144e8394
BBrannick <bbrannick@gocardless.com>
2025-12-05 19:36:19 +0000

    Use iterative solution to Day04

--- 2025/app/Day04.hs
+++ 2025/app/Day04.hs
@@ -6,19 +6,27 @@ import Data.Maybe
 type Grid = M.Map (Int,Int) Char
 
 pt1 :: String -> Int
-pt1 ls = length . filter (accessible grid) $ rollCoords
+pt1 ls = sizeDiff grid (clearRolls grid)
   where grid = build ls
-        rollCoords = M.keys $ grid
 
 pt2 :: String -> Int
-pt2 = allRolls . build
+pt2 = sum . takeWhile (>0) . mapAdjacent sizeDiff . iterate clearRolls . build
 
+sizeDiff :: Grid -> Grid -> Int
+sizeDiff m1 m2 = M.size m1 - M.size m2
+
+{-
+First approach, replaced with iterative solution
 allRolls :: Grid -> Int
 allRolls g
   | found == 0 = 0
   | otherwise = (length rolls) + allRolls g'
   where (rolls, g') = M.partitionWithKey (\k _ -> accessible g k) g
         found = length rolls
+  -}
+
+clearRolls :: Grid -> Grid
+clearRolls g = M.filterWithKey (\k _ -> not (accessible g k)) g
 
 accessible :: M.Map (Int,Int) Char -> (Int,Int) -> Bool
 accessible g (x,y) = (<4) . length
@@ -32,4 +40,7 @@ build = M.fromList
 
 nbrs :: (Int,Int) -> [(Int,Int)]
 nbrs (x,y) = [(x+i,y+j) | i <- [-1..1], j <- [-1..1], (i,j) /= (0,0)]
+
+mapAdjacent :: (a -> a -> b) -> [a] -> [b]
+mapAdjacent f xs = zipWith f xs (tail xs)
 
