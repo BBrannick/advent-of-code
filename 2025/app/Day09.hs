18d996de831c979e16ddd4cd66f123f9692483da
BBrannick <bbrannick@gocardless.com>
2025-12-13 12:53:58 +0000

    Day 09 part 1 solution

---
+++ 2025/app/Day09.hs
@@ -0,0 +1,22 @@
+module Day09 (pt1, pt2) where
+
+import Data.List.Split
+
+pt1 :: String -> Int
+pt1 = maximum . map (uncurry square) . allPairs . map parse . lines
+
+pt2 :: String -> Int
+pt2 = length
+
+square :: (Int,Int) -> (Int,Int) -> Int
+square (x,y) (x',y') = (abs (x' - x) + 1) * (abs (y' - y) + 1)
+
+allPairs :: [a] -> [(a,a)]
+allPairs [] = []
+allPairs (x:xs) = ((repeat x) `zip` xs) ++ allPairs xs
+
+
+parse :: String -> (Int,Int)
+parse ls = case (map (read::String->Int) . splitOn "," $ ls) of 
+             (x:y:_) -> (x,y)
+             _ -> error "bad parse"
--- 2025/app/Main.hs
+++ 2025/app/Main.hs
@@ -1,11 +1,11 @@
 module Main where
 
-import Day07 (pt1, pt2)
+import Day09 (pt1, pt2)
 
 main :: IO ()
 main = do
-  demoData <- readFile "data/07_demo.txt"
-  realData <- readFile "data/07.txt"
+  demoData <- readFile "data/09_demo.txt"
+  realData <- readFile "data/09.txt"
   print $ pt1 demoData
   print $ pt1 realData
   print $ pt2 demoData
